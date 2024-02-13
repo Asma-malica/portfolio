34b2fd62ed8d3a730943e04dd52215214c625b96
Asma-malica <115386995+Asma-malica@users.noreply.github.com>
2024-02-14 01:19:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import About from "./components/About/about" ;
 import Education from "./components/Education/education" ;
 
 import Project from "./components/Project/project" ;
-// import Contact from "./components/Contact/contact";
+import Contact from "./components/Contact/contact";
 
 import Footer from "./components/Footer/footer" ;
 import Skills from "./components/Skills/skills" ;
+
 
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
       <Education/>
       <Skills/>
       <Project/>
-      
+      <Contact/>
       
       <Footer/>
     </div>
